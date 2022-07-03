02f7d0f5a495fc8a9829f9347e81f12385ea9231
Yuliya <kalyuzhnaja.iulia@yandex.ru>
2022-07-03 21:46:18 +0300

    решение задач 10, 13, 15

---
+++ 10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("введите трехзначное число: ");
+int num = Convert.ToInt16(Console.ReadLine());
+int secondNum = (num / 10) % 10;
+Console.WriteLine(secondNum);
+
+// int something(int start, int finish) //высветить среднюю цифру из рандомного трехзначного числа
+// {
+//     int numberA = new Random().Next(start, finish);
+//     Console.WriteLine(numberA);
+//     int secondNumber = (numberA / 10) % 10;
+//     return secondNumber;
+// }
+// int num = something(100, 999);
+// Console.WriteLine(num);
---
+++ 13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.WriteLine("введите число");
+void hasThirdNumber(int x){
+    int div =1;
+    while (x/div>=10){
+        div*=10;
+    }
+    if (div <100){
+        Console.WriteLine("слишком маленькое число");
+    } else{
+        int digit = (x/(div/100))%10;
+        Console.WriteLine("третья цифра {0}", digit);
+    }
+}
+hasThirdNumber(Convert.ToInt32(Console.ReadLine()));
